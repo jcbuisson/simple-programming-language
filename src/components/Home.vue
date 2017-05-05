52283a23beeb82ab9bfb867b195da7a54eb2b398
Chris <buisson@enseeiht.fr>
2017-05-05 16:17:29 +0200

    Main layout created

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,15 +10,32 @@
 
 <div class="home">
     <div class="header-panel">
-       Header
+       <b-button-group class="header-panel-group">
+          <b-button variant="primary">Simple Programming Language Sandbox</b-button>
+       </b-button-group>
+       <b-button-group class="header-panel-group">
+          <b-button variant="success">Online version</b-button>
+          <b-button variant="primary">Documentation</b-button>
+          <b-button variant="info">Development</b-button>
+       </b-button-group>
     </div>
     
     <div class="main-panel">
        <div class="main-panel-left">
-          LEFT
+          <div class="main-panel-left-center">
+             CODE EDITOR
+          </div>
+          <div class="main-panel-left-footer">
+             DEBUG TOOLBAR
+          </div>
        </div>
        <div class="main-panel-right">
-          RIGHT
+          <div class="main-panel-right-top">
+             MEMORY
+          </div>
+          <div class="main-panel-right-bottom">
+             INPUT/OUTPUT
+          </div>
        </div>
     </div>
 </div>
@@ -87,11 +104,21 @@
 
 .header-panel {
    background: rgba(255, 0, 0, .1);
+   display: flex;
+   flex-direction: row;  
+   justify-content: space-around;
+
+   align-items: center;
+   align-content: center;
+}
+
+.header-panel-group {
+   flex: 1;
 }
 
 .main-panel {
    background: rgba(0, 255, 0, .1);
-   flex: 1 1 auto;
+   flex: 1;
 
    display: flex;
    flex-direction: row;  
@@ -102,13 +129,46 @@
 }
 
 .main-panel-left {
-   flex: 1 1 auto;
+   flex: 1;
    background: rgba(255, 255, 0, .1);
+
+   display: flex;
+   flex-direction: column;  
+   justify-content: flex-start;
+
+   align-items: stretch;
+   align-content: stretch;
+}
+
+.main-panel-left-center {
+   flex: 1;
+   background: rgba(100, 0, 100, .1);
+}
+
+.main-panel-left-footer {
+   background: rgba(100, 100, 0, .1);
 }
 
 .main-panel-right {
-   flex: 1 1 auto;
+   flex: 1;
    background: rgba(0, 255, 255, .1);
+
+   display: flex;
+   flex-direction: column;  
+   justify-content: flex-start;
+
+   align-items: stretch;
+   align-content: stretch;
+}
+
+.main-panel-right-top {
+   flex: 1;
+   background: rgba(50, 0, 100, .1);
+}
+
+.main-panel-right-bottom {
+   flex: 1;
+   background: rgba(100, 0, 50, .1);
 }
 
 </style>
